b85802c22927f85155f2c74081bace50cf407f68
Srijan Saha <107436802+srijan3691@users.noreply.github.com>
2023-03-03 11:57:04 +0530

    Add files via upload

---
+++ sr.js
@@ -0,0 +1,20 @@
+let typeData = new Typed(".role" , {
+    strings:[
+        "Counter Strike 1.6",
+        "TecHETC 2k23"
+    ],
+    loop:true,
+    typeSpeed: 120,
+    backDelay: 4000,
+    backSpeed: 70,
+    })
+    let typeData1 = new Typed(".role1" , {
+        strings:[
+            "Case, Capsule, Kit, Oh My!",
+            "version 8.0"
+        ],
+        loop:true,
+        typeSpeed: 120,
+        backDelay: 3300,
+        backSpeed: 70,
+        })
